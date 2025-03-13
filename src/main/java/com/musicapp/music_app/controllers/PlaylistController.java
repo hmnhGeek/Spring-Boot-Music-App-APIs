08c7cc8ee5e68c3e11c61f3508e84fbeb6b9acce
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-13 18:46:18 +0530

    Playlist API for creating a new playlist.

--- src/main/java/com/musicapp/music_app/controllers/PlaylistController.java
+++ src/main/java/com/musicapp/music_app/controllers/PlaylistController.java
@@ -1,57 +1,60 @@
-//package com.musicapp.music_app.controllers;
-//
-//import DTOs.requests.AddPlaylistRequestDTO;
-//import DTOs.requests.AddSongToPlaylistRequestDTO;
-//import DTOs.requests.PasswordRequestDTO;
-//import DTOs.responses.PlaylistResponseDTO;
-//import DTOs.responses.SongsListItem;
-//import com.musicapp.music_app.model.Playlist;
-//import com.musicapp.music_app.model.Song;
-//import com.musicapp.music_app.services.CredentialService;
-////import com.musicapp.music_app.services.PlaylistService;
-//import io.swagger.v3.oas.annotations.Operation;
-//import io.swagger.v3.oas.annotations.responses.ApiResponse;
-//import io.swagger.v3.oas.annotations.responses.ApiResponses;
-//import org.bson.types.ObjectId;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@RestController
-//@RequestMapping("/api/playlists")
-//@CrossOrigin(origins = "*")
-//public class PlaylistController {
-//    @Autowired
-//    private PlaylistService playlistService;
-//
-//    @Autowired
-//    private CredentialService credentialService;
-//
-//    @Operation(summary = "Create new playlist")
-//    @ApiResponses(value = {
-//            @ApiResponse(responseCode = "201", description = "Playlist created successfully"),
-//            @ApiResponse(responseCode = "500", description = "Internal server error while creating the playlist")
-//    })
-//    @PostMapping(value = "/create-playlist", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<Playlist> createPlaylist(
-//            @RequestBody AddPlaylistRequestDTO addPlaylistRequestDTO
-//    ) {
-//        try {
-//            // Call the service method to upload and encrypt the song and cover image
-//            Playlist playlist = playlistService.createPlaylist(addPlaylistRequestDTO);
-//            return new ResponseEntity<>(playlist, HttpStatus.CREATED);
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
-//
+package com.musicapp.music_app.controllers;
+
+import DTOs.requests.AddPlaylistRequestDTO;
+import DTOs.requests.AddSongToPlaylistRequestDTO;
+import DTOs.requests.PasswordRequestDTO;
+import DTOs.responses.PlaylistResponseDTO;
+import DTOs.responses.SongsListItem;
+import com.musicapp.music_app.DTO.Requests.User.CreateUserRequestDTO;
+import com.musicapp.music_app.model.Playlist;
+import com.musicapp.music_app.model.Song;
+import com.musicapp.music_app.model.User;
+import com.musicapp.music_app.services.CredentialService;
+//import com.musicapp.music_app.services.PlaylistService;
+import com.musicapp.music_app.services.PlaylistService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/playlists")
+@CrossOrigin(origins = "*")
+public class PlaylistController {
+    @Autowired
+    private PlaylistService playlistService;
+
+    @Operation(summary = "Create new playlist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Playlist created successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal server error while creating the playlist")
+    })
+    @PostMapping(value = "/create-playlist", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createUser(@RequestParam("title") String title,
+                                        @RequestParam("description") String description,
+                                        @RequestPart("coverImagePath") MultipartFile coverImagePath) {
+        try {
+            AddPlaylistRequestDTO playlist = new AddPlaylistRequestDTO();
+            playlist.setTitle(title);
+            playlist.setDescription(description);
+            Playlist savedPlaylist = playlistService.createPlaylist(playlist, coverImagePath);
+            return new ResponseEntity<>(savedPlaylist, HttpStatus.CREATED);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 //    @Operation(summary = "Add song to a playlist")
 //    @ApiResponses(value = {
 //            @ApiResponse(responseCode = "201", description = "Song added successfully"),
@@ -114,4 +117,4 @@
 //    ) {
 //        playlistService.deletePlaylist(playlistId, password);
 //    }
-//}
+}
